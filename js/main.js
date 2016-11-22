a8e4d6b7788e11b6483409f66c3ca3fa8a6ead68
yangger6 <18902833810@163.com>
2016-11-22 13:36:50 +0800

    修改产品介绍页面

--- js/main.js
+++ js/main.js
@@ -16,10 +16,29 @@ $(document).ready(function(){
             alignment: 'left' // Displays dropdown with edge aligned to the left of button
         }
     );
-    $("#main").css("height",$(".contain")[0].scrollHeight+$(".slider")[0].scrollHeight+"px");
     $("#back-to-top").fadeOut(1500);
 });
 window.onload=function () {
+    $(function(){
+        //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+        $(function () {
+            $(window).scroll(function(){
+                if ($(window).scrollTop()>100){
+                    $("#back-to-top").fadeIn(1500);
+                }
+                else
+                {
+                    $("#back-to-top").fadeOut(1500);
+                }
+            });
 
+            //当点击跳转链接后，回到页面顶部位置
+
+            $("#back-to-top").click(function(){
+                $('body,html').animate({scrollTop:0},1000);
+                return false;
+            });
+        });
+    });
 };
 
